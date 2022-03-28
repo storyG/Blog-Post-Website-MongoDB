e33e5db8cbb1d0a4238722302a8c047894d30c88
Frank Gu <frank40609@gmail.com>
2022-03-27 21:03:37 -0700

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb+srv://frank40609:sygu@cluster0.zew3s.mongodb.net/blogDB");
+mongoose.connect("mongodb+srv://frank40609:<>@cluster0.zew3s.mongodb.net/blogDB");
 
 const postSchema = {
   title: {
